dc70fc757bfbaadc6af60be43814a226054f15d2
Berkay AY <61064047+shadesofdeath@users.noreply.github.com>
2023-03-09 23:53:01 +0300

    Update 1. Gün - 8 Mart 2023.py

--- 1. Gün - 8 Mart 2023.py
+++ 1. Gün - 8 Mart 2023.py
@@ -1,3 +1,18 @@
+# Python programlama dilinde kullanılan veri tipleri şunlardır:
+
+# Sayılar (Numbers): Sayılar iki ana kategoriye ayrılır: tam sayılar (integers) ve ondalık sayılar (floats). Tam sayılar bir virgülden önceki herhangi bir sayıdır, örneğin: 2, 3, 4, 1000. Ondalık sayılar ise virgülden sonra en az bir sayı içerir, örneğin: 2.0, 3.14, 0.25.
+
+# Dizeler (Strings): Dizeler, karakterlerin bir araya gelmesiyle oluşan metinsel verilerdir. Örneğin: "Merhaba", "Python".
+
+# Listeler (Lists): Listeler, bir araya getirilmiş verilerin bir koleksiyonudur. Listeler içinde farklı veri tipleri bulunabilir. Örneğin: [1, 2, 3], ["merhaba", "dünya"], [1, "iki", 3.0].
+
+# Demetler (Tuples): Demetler, bir araya getirilmiş verilerin bir koleksiyonudur. Ancak, listelerden farklı olarak, demetler değiştirilemez (immutable) veri tipleridir. Örneğin: (1, 2, 3), ("merhaba", "dünya"), (1, "iki", 3.0).
+
+# Sözlükler (Dictionaries): Sözlükler, anahtar-değer çiftleri şeklinde verileri saklarlar. Anahtarlar genellikle bir dize (string) şeklinde kullanılır ve değerler değişebilir (mutable) veri tipleri olabilir. Örneğin: {"isim": "Ahmet", "yaş": 25, "sevdiği_renkler": ["mavi", "yeşil"]}.
+
+# Kümeler (Sets): Kümeler, benzersiz elemanları içeren veri tipleridir. Kümelerdeki elemanlar değiştirilebilir (mutable) veri tipleri olabilir. Örneğin: {1, 2, 3}, {"merhaba", "dünya"}.
+
+
 # Değişken olarak örnek vermek gerekirse kodlama.io sitesinde kursların kategorilere ayrıldığı
 # bölümlerde hepsinde "Ücretsiz" yazısı yazıyor buradaki ücretsiz yazısı bir yerde tanımlanmış
 # bir değişken olabilir.
